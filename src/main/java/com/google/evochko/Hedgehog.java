ca4697172eefab95a52f29ec3f86e510fccdaab1
Sergey Yevochko <sevochko@integrity.com.ua>
2017-03-20 18:21:31 +0200

    fix pom + add HedgehogWrapper class

--- src/main/java/com/google/evochko/Hedgehog.java
+++ src/main/java/com/google/evochko/Hedgehog.java
@@ -1,44 +1,34 @@
 package com.google.evochko;
 
+import java.io.File;
+
 public class Hedgehog {
     private int[][] apples;
     private int[][] weights;
 
-    public static void main(String[] args) {
-        Hedgehog hed = new Hedgehog();
-        hed.apples = new int[][]{{3, 4, 2}, {21, 1, 9}, {12, 5, 4}, {1, 20, 3}};
-        hed.printArray(hed.apples);
-        System.out.println(hed.getMaxApplesCount());
-        hed.printArray(hed.weights);
+    public int[][] getWeights() {
+        return weights;
     }
 
-    public void printArray(int arr[][]) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + "\t");
-            }
-            System.out.println();
-        }
+    public int[][] getApples() {
+        return apples;
     }
 
     public void setApples(int[][] apples) {
         this.apples = apples;
-        weights = new int[apples.length][apples[0].length];
     }
 
     public int getMaxApplesCount() {
-        weights = new int[apples.length][apples[0].length];
+        weights = new int[apples.length][];
         for (int i = 0; i < apples.length; i++) {
+            weights[i] = new int[apples[i].length];
             for (int j = 0; j < apples[i].length; j++) {
-                int lw = (j - 1) >= 0 ? weights[i][j - 1] : 0;
-                int uw = (i - 1) >= 0 ? weights[i - 1][j] : 0;
+                int lw = (j - 1 >= 0 && weights[i].length > j - 1) ? weights[i][j - 1] : 0;
+                int uw = (i - 1 >= 0 && weights[i - 1].length > j) ? weights[i - 1][j] : 0;
                 weights[i][j] = apples[i][j] + ((lw >= uw) ? lw : uw);
             }
         }
-        return weights[apples.length - 1][apples[0].length - 1];
-    }
-
-    public String getPath() {
-        return "";
+        int lastI = apples.length - 1;
+        return weights[lastI][apples[lastI].length - 1];
     }
 }
---
+++ src/main/java/com/google/evochko/HedgehogWrapper.java
@@ -0,0 +1,100 @@
+package com.google.evochko;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class HedgehogWrapper {
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("You have to point 2 input params: input and output file, like this:");
+            System.err.println("HedgehogWrapper.class d:/temp/in.txt d:/temp/out.txt");
+            return;
+        }
+
+        String inFile = args[0];
+        String outFile = args[1];
+        Path in = Paths.get(inFile);
+
+        if (Files.notExists(in)) {
+            System.err.println("File not found: " + inFile);
+            return;
+        }
+
+        int[][] apples = getIntArrayFromFile(in);
+        if (apples == null) {
+            System.err.println("Can't create int array from file: " + inFile);
+            return;
+        }
+
+        Hedgehog hed = new Hedgehog();
+        hed.setApples(apples);
+        System.out.println("Apples:");
+        printArray(hed.getApples());
+
+        int maxApplesCount = hed.getMaxApplesCount();
+        System.out.println("Weights:");
+        printArray(hed.getWeights());
+        System.out.println("Max apple count is " + maxApplesCount);
+
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(outFile), StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING)) {
+            writer.write(String.valueOf(maxApplesCount));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int[][] getIntArrayFromFile(Path in) {
+        try {
+            List<String> rows = Files.readAllLines(in);
+            int apples[][] = new int[rows.size()][];
+            int i = 0;
+            int maxColumn = 0;
+            for (String nums : rows) {
+                nums = Pattern.compile("\\s+").matcher(nums.trim()).replaceAll("-");
+                String[] counts = nums.split("-");
+                if (counts.length > maxColumn) {
+                    maxColumn = counts.length;
+                }
+                apples[i] = new int[counts.length];
+                int j = 0;
+                for (String count : counts) {
+                    int n = Integer.valueOf(count);
+                    apples[i][j] = n;
+                    j++;
+                }
+                i++;
+            }
+
+            int[][] retArray = new int[apples.length][maxColumn];       // we need to align columns count
+            for (int k = 0; k < apples.length; k++) {
+                for (int n = 0; n < apples[k].length; n++) {
+                    retArray[k][n] = apples[k][n];
+                }
+            }
+            return retArray;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static void printArray(int arr[][]) {
+        if (arr != null) {
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    System.out.print(arr[i][j] + "\t");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
